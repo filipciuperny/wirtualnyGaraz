59167ca2a52c36cb7f0d440c656586767a000573
filipciuperny <xinkyy7@gmail.com>
2026-03-26 12:04:10 +0100

    zadanie 1 chyba

--- auto.java
+++ auto.java
@@ -1,12 +1,3 @@
-class zadanie1{
-	public static void main(String args[]){
-		auto opel = new auto("opel", "zafira", 1999);
-		auto rdza = new auto("mazda", "323b", 2000);
-		
-		rdza.otomoto();	
-	}
-
-}
 class auto{
 	public String marka;
 	public String model;
--- main.java
+++ main.java
@@ -1,4 +1,4 @@
-class zadanie1{
+class main{
 	public static void main(String args[]){
 		auto opel = new auto("opel", "zafira", 1999);
 		auto rdza = new auto("mazda", "323b", 2000);
@@ -11,6 +11,12 @@ class auto{
 	public String marka;
 	public String model;
 	public int RokProd;
+	
+	public auto(String marka, String model, int Rokprod){
+	this.marka = marka;
+	this.model = model;
+	RokProd = Rokprod;
+	}
 
 
 	public void otomoto(){
